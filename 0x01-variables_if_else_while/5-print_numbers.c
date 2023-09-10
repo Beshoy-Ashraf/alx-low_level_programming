e2fc48b2d52d5a5fd8e142fc9a58b8b29fd84b20
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2023-09-10 03:04:04 -0700

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints the alphabet in lowercase, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int b = 10, a = 0;
+
+	while (a < b)
+	{
+		printf(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
