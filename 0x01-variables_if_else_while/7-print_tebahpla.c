5b9797cde7721e222954392cebc04d503a98af77
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2023-09-10 03:23:27 -0700

    add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints the alphabet in lowercase, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int b = 26, a = 0;
+
+	while (a < b)
+	{
+		putchar("zyxwvutsrqponmlkjihgfedcba"[a]);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
