176567005be34ad5ea608f2f2a800fcc0add276d
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2023-09-14 13:15:34 -0700

    add

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most - function that prints 0 to 9
+ * Return: 0 (Success)
+ */
+void print_most(void)
+{
+	int letter;
+
+	for (letter = 48; letter <= 57; letter++)
+	{
+		if((letter != 50) && (letter != 52 ))
+			_putchar(letter);
+	}
+	_putchar(10);
+}
