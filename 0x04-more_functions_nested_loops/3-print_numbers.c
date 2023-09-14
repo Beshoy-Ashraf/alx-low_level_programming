87059e9a5e71dba0fe686a4d3352f59d31f405bb
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2023-09-14 13:00:50 -0700

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints 0 to 9
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	char c = 48;
+
+	for (int i = c; i < 58; i++)
+		_putchar(i);
+	_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,5 @@
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+int _putchar(char c);
 #endif /* MAIN_H */
