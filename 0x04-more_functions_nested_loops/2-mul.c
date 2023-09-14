577ba64c3830a1f29298e2a1eb9db0fccf274fc9
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2023-09-14 12:43:15 -0700

    add

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,9 @@
  * _mal - function that multiplies two integers.
  * @x: input
  * @y: input
- * Return: 0 (Success)
+ * Return: a * b (Success)
  */
 int _mal(int x, int y)
 {
-	print("%d\n", x * y);
-	return (0);
+	return (a * b);
 }
